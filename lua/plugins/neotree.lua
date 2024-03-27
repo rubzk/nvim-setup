25fd8402377c6df550f57f827732a62bcb096673
rubzk <tomas.ertola@n26.com>
2024-03-27 09:50:07 +0100

    Added auto close and neotree settings

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,13 +1,17 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", 
-      "MunifTanjim/nui.nvim",
-    },
-    config = function()
-      vim.keymap.set('n', '<leader>e', ':Neotree filesystem reveal left<CR>', {})
-    end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			hide_gitignored = false,
+			hide_dotfiles = false,
+			visible = true,
+		})
+		vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>", {})
+	end,
 }
-
