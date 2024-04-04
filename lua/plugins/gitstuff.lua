74b7a0309681ecc1f8166be637a31ccacbfd7591
rubzk <tomas.ertola@gmail.com>
2024-04-04 09:38:15 +0200

    Toggle line blame

--- lua/plugins/gitstuff.lua
+++ lua/plugins/gitstuff.lua
@@ -1,12 +1,15 @@
 return {
-  {
-    "tpope/vim-fugitive",
-  },
-  {"lewis6991/gitsigns.nvim", 
-    config = function()
-      require("gitsigns").setup()
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				current_line_blame = true,
+			})
 
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-    end,}
-
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+		end,
+	},
 }
