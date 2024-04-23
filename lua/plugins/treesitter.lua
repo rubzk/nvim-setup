90e9b16b3c72269c5df756724075cf385ea4193a
rubzk <tomas.ertola@n26.com>
2024-04-23 20:44:28 +0200

    Added aerial

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,14 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-    ensure_installed = {"lua","python","vim","query"},
-    highlight = {enable = true},
-    ident = { enable = true},
-      })
-  end
+	{
 
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = { "lua", "python", "vim", "query" },
+				highlight = { enable = true },
+				ident = { enable = true },
+			})
+		end,
+	},
+	{
+		"stevearc/aerial.nvim",
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+		config = function()
+			local config = require("aerial")
+			config.setup({
+				on_attach = function(bufnr)
+					vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+					vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+				end,
+				vim.keymap.set("n", "<leader>k", "<cmd>AerialToggle!<CR>"),
+			})
+		end,
+	},
 }
- 
