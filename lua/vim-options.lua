8d6c71a34c49d517c5b97f63defb875e4f651937
rubzk <tomas.ertola@gmail.com>
2024-04-24 17:29:58 +0200

    Yank to clipboard

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -32,4 +32,14 @@ vim.api.nvim_set_keymap("n", "<C-u>", "<C-u>zz", { noremap = true }) -- Same
 
 vim.api.nvim_set_keymap("n", "n", "nzzzv", { noremap = true }) -- Same 
 vim.api.nvim_set_keymap("n", "N", "Nzzzv", { noremap = true }) -- Same 
+-- Yank into system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y') -- yank motion
+vim.keymap.set({'n', 'v'}, '<leader>Y', '"+Y') -- yank line
 
+-- Delete into system clipboard
+vim.keymap.set({'n', 'v'}, '<leader>d', '"+d') -- delete motion
+vim.keymap.set({'n', 'v'}, '<leader>D', '"+D') -- delete line
+
+-- Paste from system clipboard
+vim.keymap.set('n', '<leader>p', '"+p')  -- paste after cursor
+vim.keymap.set('n', '<leader>P', '"+P')  -- paste before cursor
