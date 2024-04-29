cf48a9f684112d6a2865bb4ea125520fb93ad473
rubzk <tomas.ertola@n26.com>
2024-04-29 17:46:49 +0200

    Tsx

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "pyright", "gopls", "astro"},
+				ensure_installed = { "lua_ls", "pyright", "gopls", "astro", "tsserver" },
 			})
 		end,
 	},
@@ -18,7 +18,8 @@ return {
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-      lspconfig.astro.setup({})
+			lspconfig.astro.setup({})
+			lspconfig.tsserver.setup({})
 			lspconfig.lua_ls.setup({
 				capabilities = capabilities,
 			})
